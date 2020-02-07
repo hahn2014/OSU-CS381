f487574b2cdaccf420853d58f65683e8033a0d91
Hahn2014 <slapshot1998@gmail.com>
2020-02-06 19:56:01 -0800

    Added functional comments and psuedocode for extra credit problems.

--- HW3.hahnb.hs
+++ HW3.hahnb.hs
@@ -3,7 +3,6 @@
    --     Sheldon Roberts robeshel@oregonstate.edu
 module MiniLogo where
 import Data.List
-import Data.Stack
 import Data.Typeable
 
 -- num	::=	(any natural number)             --Integers
@@ -78,16 +77,14 @@ steps_helper n [x, y] = [Call "line" [Variable "x", Variable "y", Variable "x", 
     Call "line" [Variable "x", Add (Variable "y") (Number 1), Add (Variable "x") (Number 1), Add (Variable "y") (Number 1)] ] ++ steps_helper (n - 1) [x + 1, y + 1]
 
 
--- define macros(Prog, [Macros]) {
---
--- };
+-- case match for specifically Definitions only, otherwise go to next recursive call
 macros :: Prog -> [Macro]
 macros []     = []
 macros (x:xs) = case x of
     Def m _ _ -> m : macros xs
     otherwise -> macros xs
 
-
+-- case match for each command type in the program and call respective helper functions
 pretty :: Prog -> String
 pretty []     = ""
 pretty (x:xs) = case x of
@@ -96,63 +93,46 @@ pretty (x:xs) = case x of
     Def m v p           -> show ("Def " ++ m ++ " ") ++ prettyVars v ++ pretty p ++ "\n" ++ pretty xs
     Call m e            -> show ("Call " ++ m ++ " ") ++ prettyExpression e ++ "\n" ++ pretty xs
 
-
+-- takes a pen mode and returns Up or Down respectively
 prettyPen :: Mode -> String
 prettyPen Up   = "Up"
 prettyPen Down = "Down"
 
-
+-- takes a list of expressions and returns a printable string
 prettyExpression :: [Expr] -> String
 prettyExpression []     = ""
-prettyExpression (e:es) = case e of
+prettyExpression (e:es) = case e of -- case match for Expression Type
     Number i -> show i ++ ", " ++ prettyExpression es
     Variable v -> v ++ ", " ++ prettyExpression es
     Add e1 e2 -> prettyExpression [e1] ++ " + " ++ prettyExpression [e2] ++ ", " ++ prettyExpression es
 
 
+-- takes a list of variables and returns a printable string
 prettyVars :: [Var] -> String
 prettyVars []     = ""
 prettyVars (v:vs) = show v ++ prettyVars vs
 
 
-
+-- How to solve
+-- we were unable to import Data.Stack to attempt to push and pop on the stack.
+--      Planning on putting it into RPN to then perform stack arithmatic on the
+--      literal values. Seperate stack would be used to store all non-literal values
+--      to be merged at the end.
 optE :: Expr -> Expr
-optE = undefined --Add e1 e2 =
+optE = undefined
+-- optE (Add e1 e2) =
 --     if (typeOf (e1) == Number && typeOf (e2) == Number)
---         then stackPop (stackPush (e1))
---     else
+--         then (e1 + e2)
+--     else show ""
+-- optE otherwise = otherwise
 
 
-
-
+-- How to solve
+-- case match through all commands in the program, then patter match all possible
+--      command types that contain expressions (ie. Move, Def, and Call), then
+--      call optE to simplify any found literal expressions.
 optP :: Prog -> Prog
-optP = undefined;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-__
+optP = undefined -- (x:xs) = case x of
+--     Move e1 e2 =
+--     Def m v p  =
+--     Call m e   =
